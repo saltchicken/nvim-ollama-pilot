e221eba48f4eeec149fce68a21dc47edecfa2da6
saltchicken <john.eicher89@gmail.com>
2024-09-16 16:04:19 -0700

    Update request function

--- lua/nvim-ollama-pilot/init.lua
+++ lua/nvim-ollama-pilot/init.lua
@@ -6,7 +6,15 @@ end
 
 ollama_pilot.request = function()
 	local request = require("nvim-ollama-pilot.request")
-	print("Hello World")
+	request.send_post_request("How is it going?", function(response, error)
+		if error then
+			print(error)
+		else
+			local json = require("nvim-ollama-pilot.json")
+			local decoded_response = json.decode(response)
+			print("Response: ", decoded_response.response)
+		end
+	end)
 end
 
 return ollama_pilot
