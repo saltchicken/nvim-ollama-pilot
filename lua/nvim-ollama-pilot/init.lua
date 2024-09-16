abef7d713794d8b39b712b819099dee3c251ae63
saltchicken <john.eicher89@gmail.com>
2024-09-16 15:34:02 -0700

    Implement setup function

--- lua/nvim-ollama-pilot/init.lua
+++ lua/nvim-ollama-pilot/init.lua
@@ -1,2 +1,12 @@
-Request = require("nvim-ollama-pilot.request")
-print("Hello World")
+local ollama_pilot = {}
+
+ollama_pilot.setup = function()
+	print("Setup was called")
+end
+
+ollama_pilot.request = function()
+	local request = require("nvim-ollama-pilot.request")
+	print("Hello World")
+end
+
+return ollama_pilot
