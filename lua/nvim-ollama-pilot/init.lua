bfaa853a9b4a11c2cee76186fa89e9103c0fc948
saltchicken <john.eicher89@gmail.com>
2024-09-16 20:57:54 -0700

    Fix debug

--- lua/nvim-ollama-pilot/init.lua
+++ lua/nvim-ollama-pilot/init.lua
@@ -67,7 +67,7 @@ ollama_pilot.setup = function(params)
 end
 
 ollama_pilot.request = function(prompt, guidance)
-	local test = prompt:gsub("[^%w%s%p]", "")
+	local test = prompt:gsub("[^%a]", "")
 	print(test)
 	for i = 1, #prompt do
 		local char = string.sub(prompt, i, i)
